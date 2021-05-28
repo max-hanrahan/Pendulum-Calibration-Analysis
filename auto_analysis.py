ae602910146107736c7df807ab82d90d6c836551
Max Hanrahan <43153157+max-hanrahan@users.noreply.github.com>
2021-05-28 12:30:13 -0400

    Displays linear acceleration in m/s^2

--- auto_analysis.py
+++ auto_analysis.py
@@ -27,33 +27,45 @@ all_data = np.genfromtxt(path, delimiter = '\t', skip_footer = 1)
 count = np.arange(0,1000, 1)
 timestamp = all_data[:,1]
 temp = all_data[:,2]
-x_accel = all_data[:,3]
-y_accel = all_data[:,4]
-z_accel = all_data[:,5]
-x_gyro = all_data[:,6]
-y_gyro = all_data[:,7]
-z_gyro = all_data[:,8]
+x_accel_bits = all_data[:,3]
+y_accel_bits = all_data[:,4]
+z_accel_bits = all_data[:,5]
+x_gyro_bits = all_data[:,6]
+y_gyro_bits = all_data[:,7]
+z_gyro_bits = all_data[:,8]
 
-'The hardest part of the fitting is the guessing.'
-'You might have to run it many times, manually adjusting these parameters every time.'
+# constants:
+g = 9.80665 # from https://physics.nist.gov/cgi-bin/cuu/Value?gn
 
-# parameter guesses
+# these data were taken with the device stationary atop the workbench
+# we therefore have a guess calibration factor
+M_S_2_PER_BIT = g/np.average(z_accel_bits)
+
+# so for linear acceleration, we can convert the bits to m/s^2
+x_accel = x_accel_bits*M_S_2_PER_BIT
+y_accel = y_accel_bits*M_S_2_PER_BIT
+z_accel = z_accel_bits*M_S_2_PER_BIT
+
+'''
+The hardest part of the fitting is the guessing.
+You might have to run it many times, manually adjusting these parameters every time.
+'''
+
+### parameter guesses
 THETA_0 = np.radians(float(input('initial angle guess (deg): ')))
 k_guess = float(input('k-factor guess (for linear damping): '))
 frequency = float(input('guess angular frequency: '))
-
-# constants:
-g = 9.80
 
 # define functions designed for scipy's curve fit that can guess omega
 
 def x_accel_to_fit(t, frq, theta_init):
     # optimizes fit for linear acceleration in the x-direction by changing theta and omega
-    return g*np.cos(theta_init * np.cos(frq * t)) + np.average(x_accel) # add mean for better fit
+    return (g*np.cos(theta_init * np.cos(frq * t)))*M_S_2_PER_BIT + np.average(x_accel) # add mean for better fit
 
 def theta_accel(t, omega, k_factor):
     # optimizes the fit gyroscopic acceleration by changing omega and k
-    return -THETA_0 * omega**2 * np.cos(omega*t) * np.exp(-k_factor * t) + np.average(z_gyro) # add mean for better fit
+    return -THETA_0 * omega**2 * np.cos(omega*t) * np.exp(-k_factor * t) + np.average(z_gyro_bits) # add mean for better fit
+
 
 # this is where the actual fitting begins
 parameters0 = [frequency, THETA_0]
@@ -62,7 +74,7 @@ parameters1 = [frequency, k_guess]
 # popt is the list of optimal parameters, as determined by curve_fit
 # pcov is the covariance matrix, whose diagonal entries are the variance of each parameter
 popt0, pcov0 = curve_fit(x_accel_to_fit, timestamp, x_accel, parameters0)
-popt1, pcov1 = curve_fit(theta_accel, timestamp, z_gyro, parameters1)
+popt1, pcov1 = curve_fit(theta_accel, timestamp, z_gyro_bits, parameters1)
 
 accel_fit = x_accel_to_fit(timestamp, *parameters0)
 rad_accel_fit = theta_accel(timestamp, *parameters1)
@@ -73,7 +85,7 @@ ax[0].legend()
 ax[0].set_xlabel('Timestamp (sec)')
 ax[0].set_ylabel('Acceleration (bits)')
 
-ax[1].plot(timestamp, z_gyro, c = 'dodgerblue', label = 'ang. accel data')
+ax[1].plot(timestamp, z_gyro_bits, c = 'dodgerblue', label = 'ang. accel data')
 ax[1].plot(timestamp, rad_accel_fit, c= 'orangered', label = 'fit')
 ax[1].legend()
 ax[1].set_xlabel('Timestamp (sec)')
